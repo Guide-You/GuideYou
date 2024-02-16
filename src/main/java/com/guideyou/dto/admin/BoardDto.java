a2b133a64fc21a89eb85a484b60a065b5b0072b9
tngus78901 <tngus78901@gmail.com>
2024-02-16 18:04:38 +0900

    test입니다

---
+++ src/main/java/com/guideyou/dto/admin/BoardDto.java
@@ -0,0 +1,26 @@
+package com.guideyou.dto.admin;
+
+import java.sql.Timestamp;
+/**
+  * @FileName : BoardDto.java
+  * @Project : GuideYou
+  * @Date : 2024. 2. 16. 
+  * @작성자 : 김수현
+  * @변경이력 :
+  * @프로그램 설명 : 
+  */
+public class BoardDto {
+
+	int boardNo;
+	String writer;
+	String title;
+	String content;
+	int viewCount;
+	Timestamp createdAt;
+
+
+
+
+
+
+}
