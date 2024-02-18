ff6e9c2d966dffb70f5f81e2a47a4d0450c5d6b3
cwal8202 <104574997+cwal8202@users.noreply.github.com>
2024-02-18 17:15:08 +0900

    네이버 로그인끝(카카오, 구글 할려면 코드 수정 해야함)

--- src/main/java/com/guideyou/dto/UserDto.java
+++
@@ -1,5 +0,0 @@
-package com.guideyou.dto;
-
-public class UserDto {
-
-}
--- src/main/java/com/guideyou/dto/user/SignUpDTO.java
+++ src/main/java/com/guideyou/dto/user/SignUpDTO.java
@@ -1,4 +1,4 @@
-package com.guideyou.repository.entity;
+package com.guideyou.dto.user;
 
 import java.sql.Timestamp;
 
@@ -7,27 +7,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
-  * @FileName : User.java
-  * @Project : GuideYou
-  * @Date : 2024. 2. 18. 
-  * @작성자 : 최장호
-  * @변경이력 :
-  * @프로그램 설명 : user_tb 객체
-  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
-    private Integer id;
-    
+public class SignUpDTO {
     private String name;
     private String nickname;
     private String gender;
     private String email;
     private String phone;
     private String comment;
-
-    private Timestamp createdAt;
 }
