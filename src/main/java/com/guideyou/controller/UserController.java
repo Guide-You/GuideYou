f28dbe1ae4cc167bad41739bc9d2d0df79a2a282
cwal8202 <cwal8202@naver.com>
2024-02-19 10:51:54 +0900

    usercontroller 오류 수정

--- src/main/java/com/guideyou/controller/UserController.java
+++ src/main/java/com/guideyou/controller/UserController.java
@@ -22,8 +22,6 @@ import jakarta.servlet.http.HttpSession;
  */
 @Controller
 public class UserController {
-	public static void main(String[] args) {
-		System.out.println("최장호");
 
 	@Autowired
 	private UserService userService;
